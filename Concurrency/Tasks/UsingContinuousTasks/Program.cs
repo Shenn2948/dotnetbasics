cc45f1e9f30ae3c7aa2fde2b41fc124d392b1c8f
Shenn2948 <shenn2948@gmail.com>
2020-02-09 14:43:27 +0300

    UsingTasks add

--- Concurrency/Tasks/UsingContinuousTasks/Program.cs
+++ Concurrency/Tasks/UsingContinuousTasks/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-// ReSharper disable StyleCop.SA1400
 
+// ReSharper disable StyleCop.SA1400
 namespace UsingContinuousTasks
 {
     class Program
@@ -14,21 +15,21 @@ namespace UsingContinuousTasks
         public static void Main(string[] args)
         {
             string[] urls =
-            {
-                "http://microsoft.com",
-                "http://www.learnasync.net/",
-                "http://www.albahari.com/threading/",
-                "http://jonskeet.uk/csharp/threads/",
-                "https://stephencleary.com/book/",
-                "https://docs.microsoft.com/en-us/dotnet/standard/threading/the-managed-thread-pool",
-                "https://mva.microsoft.com/search/SearchResults.aspx#!q=Jeffrey%20Richter%20Threading&lang=1033",
-                "https://channel9.msdn.com/Events/Build/2012/3-011",
-                "http://asd234efwdw23reefsfdsfds.com",
-                "https://codewala.net/2015/07/29/concurrency-vs-multi-threading-vs-asynchronous-programming-explained/",
-                "http://dotnetpattern.com/multi-threading-interview-questions",
-                "https://www.pluralsight.com/courses/skeet-async",
-                "https://rsdn.org/article/dotnet/CSThreading1.xml"
-            };
+                {
+                    "http://microsoft.com",
+                    "http://www.learnasync.net/",
+                    "http://www.albahari.com/threading/",
+                    "http://jonskeet.uk/csharp/threads/",
+                    "https://stephencleary.com/book/",
+                    "https://docs.microsoft.com/en-us/dotnet/standard/threading/the-managed-thread-pool",
+                    "https://mva.microsoft.com/search/SearchResults.aspx#!q=Jeffrey%20Richter%20Threading&lang=1033",
+                    "https://channel9.msdn.com/Events/Build/2012/3-011",
+                    "http://asd234efwdw23reefsfdsfds.com",
+                    "https://codewala.net/2015/07/29/concurrency-vs-multi-threading-vs-asynchronous-programming-explained/",
+                    "http://dotnetpattern.com/multi-threading-interview-questions",
+                    "https://www.pluralsight.com/courses/skeet-async",
+                    "https://rsdn.org/article/dotnet/CSThreading1.xml"
+                };
 
             var tasks = new Task<int>[urls.Length];
 
@@ -37,66 +38,81 @@ namespace UsingContinuousTasks
                 var index = i;
 
                 // TODO Create a new task and queue it to run.
-                {
-                    Console.WriteLine("Task {0} is started. Downloading {1}", Task.CurrentId, urls[index]);
+                Task<byte[]> webClientTask = Task.Run(
+                    () =>
+                        {
+                            Console.WriteLine($"Task {Task.CurrentId} is started. Downloading {urls[index]}");
 
-                    var webClient = new WebClient();
-                    var bytes = webClient.DownloadData(urls[index]);
+                            var webClient = new WebClient();
+                            var bytes = webClient.DownloadData(urls[index]);
 
-                    Console.WriteLine("Task {0} is completed.", Task.CurrentId);
+                            Console.WriteLine($"Task {Task.CurrentId} is completed.");
 
-                    // TODO Set task result.
-                };
+                            // TODO Set task result.
+                            return bytes;
+                        });
 
-                // TODO Create a new task as a continuation of the web-client task. This task should run only if the previous task completed successfully.
-                {
-                    Console.WriteLine("Task {0} is converting bytes to string.", Task.CurrentId);
+                Task<string> getStringTask = webClientTask.ContinueWith(
+                    x =>
+                        {
+                            // TODO Create a new task as a continuation of the web-client task. This task should run only if the previous task completed successfully.
+                            Console.WriteLine($"Task {Task.CurrentId} is converting bytes to string.");
 
-                    // TODO Set the result from the previous task.
-                    byte[] bytes = null;
-                    var resultString = Encoding.UTF8.GetString(bytes);
+                            // TODO Set the result from the previous task.
+                            byte[] bytes = x.Result;
+                            string resultString = Encoding.UTF8.GetString(bytes);
 
-                    // TODO Set task result.
-                };
+                            // TODO Set task result.
+                            return resultString;
+                        });
 
-                // TODO Create a new task as a continuation of the web-client task. This task should run only if the previous task failed.
-                {
-                    // TODO Set Id of the previous task.
-                    int id = 0;
+                webClientTask.ContinueWith(
+                    x =>
+                        {
+                            if (x.IsFaulted)
+                            {
+                                // TODO Create a new task as a continuation of the web-client task. This task should run only if the previous task failed.
+                                // TODO Set Id of the previous task.
+                                int id = x.Id;
 
-                    // TODO Set inner exception message that happen in the previous task.
-                    string message = string.Empty;
+                                // TODO Set inner exception message that happen in the previous task.
+                                string message = x.Exception?.Message;
 
-                    Console.WriteLine("Task {0} failed with exception: {1}", id, message);
-                };
+                                Console.WriteLine($"Task {id} failed with exception: {message}");
+                            }
+                        });
 
-                // TODO Create a new task as a continuation of the get-string task. This task should run only if the previous task completed successfully.
-                {
-                    Console.WriteLine("Task {0} is returning string length.", Task.CurrentId);
+                tasks[index] = getStringTask.ContinueWith(
+                    x =>
+                        {
+                            // TODO Create a new task as a continuation of the get-string task. This task should run only if the previous task completed successfully.
+                            Console.WriteLine($"Task {Task.CurrentId} is returning string length.");
 
-                    // TODO Set the result from the previous task.
-                    string resultString = string.Empty;
+                            // TODO Set the result from the previous task.
+                            string resultString = x.Result;
 
-                    var occurences = IndexesOf(resultString, Token).Length;
+                            var occurences = IndexesOf(resultString, Token).Length;
 
-                    // TODO Set task result.
-                };
+                            // TODO Set task result.
+                            return occurences;
+                        });
             }
 
             Console.WriteLine("Waiting for tasks to complete.");
             try
             {
                 // TODO Use tasks array to wait until all the tasks will complete their work.
+                Task.WaitAll(tasks);
             }
             catch
             {
                 // NOTE Eat an exception here because we do not care about it here.
             }
 
-            Console.WriteLine("\nResults for searching '{0}' word:", Token);
+            Console.WriteLine($"\nResults for searching '{Token}' word:");
             for (int i = 0; i < urls.Length; i++)
             {
-                Console.WriteLine("{0} - {1}", urls[i], !(tasks[i].IsFaulted || tasks[i].IsCanceled) ? tasks[i].Result.ToString() : tasks[i].Status.ToString());
+                Console.WriteLine($"{urls[i]} - {(!(tasks[i].IsFaulted || tasks[i].IsCanceled) ? tasks[i].Result.ToString() : tasks[i].Status.ToString())}");
             }
 
             Console.WriteLine("Press any key to exit.");
@@ -111,7 +127,7 @@ namespace UsingContinuousTasks
             }
 
             List<int> indexes = new List<int>();
-            for (int index = 0; ; index += value.Length)
+            for (int index = 0;; index += value.Length)
             {
                 index = str.IndexOf(value, index);
                 if (index == -1)
--- Concurrency/Tasks/UsingTasks/Program.cs
+++ Concurrency/Tasks/UsingTasks/Program.cs
@@ -13,21 +13,21 @@ namespace UsingTasks
         public static void Main(string[] args)
         {
             string[] urls =
-            {
-                "http://microsoft.com",
-                "http://www.learnasync.net/",
-                "http://www.albahari.com/threading/",
-                "http://jonskeet.uk/csharp/threads/",
-                "https://stephencleary.com/book/",
-                "https://docs.microsoft.com/en-us/dotnet/standard/threading/the-managed-thread-pool",
-                "https://mva.microsoft.com/search/SearchResults.aspx#!q=Jeffrey%20Richter%20Threading&lang=1033",
-                "https://channel9.msdn.com/Events/Build/2012/3-011",
-                "http://asd234efwdw23reefsfdsfds.com",
-                "https://codewala.net/2015/07/29/concurrency-vs-multi-threading-vs-asynchronous-programming-explained/",
-                "http://dotnetpattern.com/multi-threading-interview-questions",
-                "https://www.pluralsight.com/courses/skeet-async",
-                "https://rsdn.org/article/dotnet/CSThreading1.xml"
-            };
+                {
+                    "http://microsoft.com",
+                    "http://www.learnasync.net/",
+                    "http://www.albahari.com/threading/",
+                    "http://jonskeet.uk/csharp/threads/",
+                    "https://stephencleary.com/book/",
+                    "https://docs.microsoft.com/en-us/dotnet/standard/threading/the-managed-thread-pool",
+                    "https://mva.microsoft.com/search/SearchResults.aspx#!q=Jeffrey%20Richter%20Threading&lang=1033",
+                    "https://channel9.msdn.com/Events/Build/2012/3-011",
+                    "http://asd234efwdw23reefsfdsfds.com",
+                    "https://codewala.net/2015/07/29/concurrency-vs-multi-threading-vs-asynchronous-programming-explained/",
+                    "http://dotnetpattern.com/multi-threading-interview-questions",
+                    "https://www.pluralsight.com/courses/skeet-async",
+                    "https://rsdn.org/article/dotnet/CSThreading1.xml"
+                };
 
             var tasks = new Task<int>[urls.Length];
 
@@ -36,19 +36,22 @@ namespace UsingTasks
                 var index = i;
 
                 // TODO Create a new task using static Task method, queue it to run, and save the task to tasks array. Use the block below as an Action delegate parameter.
-                {
-                    Console.WriteLine("Task {0} is started. Downloading {1}", Task.CurrentId, urls[index]);
+                tasks[index] = Task.Run(
+                    () =>
+                        {
+                            Console.WriteLine($"Task {Task.CurrentId} is started. Downloading {urls[index]}");
 
-                    var webClient = new WebClient();
+                            var webClient = new WebClient();
 
-                    var bytes = webClient.DownloadData(urls[index]);
-                    var resultString = Encoding.UTF8.GetString(bytes);
-                    var occurences = IndexesOf(resultString, Token).Length;
+                            var bytes = webClient.DownloadData(urls[index]);
+                            var resultString = Encoding.UTF8.GetString(bytes);
+                            var occurences = IndexesOf(resultString, Token).Length;
 
-                    Console.WriteLine("Task {0} is completed.", Task.CurrentId);
+                            Console.WriteLine($"Task {Task.CurrentId} is completed.");
 
-                    // TODO Make occurences variable a task result.
-                }
+                            // TODO Make occurences variable a task result.
+                            return occurences;
+                        });
             }
 
             Console.WriteLine("Waiting for tasks to complete.");
@@ -56,16 +59,17 @@ namespace UsingTasks
             try
             {
                 // TODO Use tasks array to wait until all the tasks will complete their work.
+                Task.WaitAll(tasks);
             }
             catch
             {
                 // NOTE Eat an exception here because we will process this exception later.
             }
 
-            Console.WriteLine("\nResults for searching '{0}' word:", Token);
+            Console.WriteLine($"\nResults for searching '{Token}' word:");
             for (int i = 0; i < urls.Length; i++)
             {
-                Console.WriteLine("{0} - {1}", urls[i], !tasks[i].IsFaulted ? tasks[i].Result.ToString() : tasks[i].Exception.InnerException.Message);
+                Console.WriteLine($"{urls[i]} - {(!tasks[i].IsFaulted ? tasks[i].Result.ToString() : tasks[i].Exception.InnerException.Message)}");
             }
 
             Console.WriteLine("Press any key to exit.");
@@ -76,11 +80,11 @@ namespace UsingTasks
         {
             if (string.IsNullOrEmpty(value))
             {
-                throw new ArgumentException("value is empty", "value");
+                throw new ArgumentException("value is empty", nameof(value));
             }
 
             List<int> indexes = new List<int>();
-            for (int index = 0; ; index += value.Length)
+            for (int index = 0;; index += value.Length)
             {
                 index = str.IndexOf(value, index);
                 if (index == -1)
